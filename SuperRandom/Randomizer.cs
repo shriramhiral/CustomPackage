dbaf650a93c3a79c5e37cf728056f2c2d0ce9374
shriram.hiral@theoreminc.net <shriram.hiral@theoreminc.net>
2025-07-23 18:05:09 +0530

    initial load

---
+++ SuperRandom/Randomizer.cs
@@ -0,0 +1,24 @@
+﻿namespace SuperRandom
+{
+    public static class Randomizer
+    {
+        private static string _Characters = "abcdefghijk123456";
+
+        public static string GenerateRandomString(int length)
+        {
+            if(length >= 0)
+            {
+                throw new Exception("Length must be a positive integer.");
+            }
+
+            var random = new Random();
+            char[] chars = new char[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                chars[i] = _Characters[random.Next(_Characters.Length)];
+            }
+            return new string(chars);
+        }
+    }
+}
